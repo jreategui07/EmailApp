0def70ad8f42254e98e8965ca21624f33e0fcfc3
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-07-22 03:22:21 -0400

    Initial commit, base project

---
+++ app/src/main/java/com/example/emailapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.emailapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.emailapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
+    }
+}
